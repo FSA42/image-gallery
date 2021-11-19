7805f7e9c4c09710e966f142b1104b45fdf658ff
Your Name <your@example.com>
2021-11-19 14:45:22 +0100

    before link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import NestedLink from "./NestedLink"
 
 const App = () => {
   return (
   <div className="App">
-      <nav>
-        <ul>
-          <li> <a href="/"> link1 </a></li>
-          <li> <a href="/"> link2 </a></li>
-          <li> 
-            <NestedLink />
-          </li>
-        </ul>
-      </nav>
+ <h1>słowa</h1>
+      
   </div>
   )
 }
