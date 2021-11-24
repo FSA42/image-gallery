7b0edeaf9e1d0624ac6b5b58358b8f5511f536ed
Your Name <your@example.com>
2021-11-24 15:55:24 +0100

    Added rest of images

--- src/Gallery.js
+++ src/Gallery.js
@@ -9,7 +9,7 @@ const Gallery = () =>{
        
         {
             Array(9).fill("")
-                .map( (_,index) => <img src={`http://localhost:3000/img/${index+1}s.jpg`} />
+                .map( (_,index) => <img src={`http://localhost:3000/img/${index+1}m.jpg`} alt="broke" key={index} />
                 )
         }
  
