6fe3967f0f38acf2419b3743c2d727aa048524a2
Your Name <your@example.com>
2021-12-03 15:07:56 +0100

    Cleared out everything

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import { useState } from 'react'
-
-import Gallery from "./Gallery"
-import ZoomedImage from './ZoomedImage'
 
 const App = () => {
-  const [ ImgID, setImgID ] = useState( null )
-
-  console.log( ImgID )
   return (  
     <div className="App">
-      {
-        ImgID === null 
-        ? <Gallery  setImgID ={setImgID}/> 
-        : <ZoomedImage setImgID={setImgID} ImgID={ImgID}/>
-      }
-           
       
     </div>
   )
